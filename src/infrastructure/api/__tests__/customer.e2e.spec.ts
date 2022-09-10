1539882b408776e9f1ddfc8d3718a0a7b263804e
Drinnn <pedro_lontro@hotmail.com>
2022-09-10 11:47:30 -0300

    test: add list customer e2e test

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -39,4 +39,54 @@ describe("E2E Test - Customer", () => {
 
     expect(response.status).toBe(500);
   });
+
+  it("should list all customers", async () => {
+    const firstCustomerCreation = await request(app)
+      .post("/customer")
+      .send({
+        name: "John",
+        address: {
+          street: "Lalau St",
+          number: 123,
+          zip: "123456",
+          city: "Lalauland",
+        },
+      });
+
+    expect(firstCustomerCreation.status).toBe(200);
+
+    const secondCustomerCreation = await request(app)
+      .post("/customer")
+      .send({
+        name: "Doe",
+        address: {
+          street: "Fafa St",
+          number: 321,
+          zip: "654321",
+          city: "Fafaland",
+        },
+      });
+
+    expect(secondCustomerCreation.status).toBe(200);
+
+    const response = await request(app).get("/customer").send();
+
+    expect(response.status).toBe(200);
+    expect(response.body.customers.length).toBe(2);
+
+    const customer1 = response.body.customers[0];
+    const customer2 = response.body.customers[1];
+
+    expect(customer1.name).toBe("John");
+    expect(customer1.address.street).toEqual("Lalau St");
+    expect(customer1.address.number).toEqual(123);
+    expect(customer1.address.zip).toEqual("123456");
+    expect(customer1.address.city).toEqual("Lalauland");
+
+    expect(customer2.name).toBe("Doe");
+    expect(customer2.address.street).toEqual("Fafa St");
+    expect(customer2.address.number).toEqual(321);
+    expect(customer2.address.zip).toEqual("654321");
+    expect(customer2.address.city).toEqual("Fafaland");
+  });
 });
