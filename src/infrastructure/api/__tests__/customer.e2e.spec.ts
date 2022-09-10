556bbfbe696adbf67bc2baf9c4f09c3155575c3d
Drinnn <pedro_lontro@hotmail.com>
2022-09-10 11:28:58 -0300

    test: add customer e2e first test

---
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -0,0 +1,34 @@
+import { app, sequelize } from "../express";
+import request from "supertest";
+
+describe("E2E Test - Customer", () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a customer", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "John",
+        address: {
+          street: "Lalau St",
+          number: 123,
+          zip: "123456",
+          city: "Lalauland",
+        },
+      });
+
+    expect(response.status).toBe(200);
+
+    expect(response.body.name).toBe("John");
+    expect(response.body.address.street).toBe("Lalau St");
+    expect(response.body.address.number).toBe(123);
+    expect(response.body.address.zip).toBe("123456");
+    expect(response.body.address.city).toBe("Lalauland");
+  });
+});
