26297008309fc42dd9bbbc18bec6341d77e0962c
Drinnn <pedro_lontro@hotmail.com>
2022-09-11 17:02:09 -0300

    feat: add notification pattern

---
+++ src/domain/@shared/notification/notification.ts
@@ -0,0 +1,22 @@
+export interface NotificationError {
+  message: string;
+  context: string;
+}
+
+export class Notification {
+  private errors: NotificationError[] = [];
+
+  public addError(error: NotificationError): void {
+    this.errors.push(error);
+  }
+
+  public messages(context?: string): string {
+    let message = "";
+    this.errors.forEach((error) => {
+      if (!context || error.context === context)
+        message += `${error.context}: ${error.message},`;
+    });
+
+    return message;
+  }
+}
